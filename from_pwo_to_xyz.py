2b180fdfae1c52a950cdb8470ed3556a7ea99eb5
Lorenzo Razzolini <lorenzo.razzolini@studio.unibo.it>
2023-11-27 15:58:05 +0100

    Fine del recupero del file from_pwo_to_xyz.py

--- from_pwo_to_xyz.py
+++ from_pwo_to_xyz.py
@@ -2,107 +2,76 @@ import re
 #da inserire eventuali altri lat par, o volume e finire gli alòtri casi
 #fai dizionario
 def ibrav(i, a):
-   if i == 0 : 
-      return '  Free'  
-   elif i == 1 :
-      return f'  Cubic (sc): V1 = {a}(1, 0, 0), V2 = {a}(0, 1, 0), V3 = {a}(0, 0, 1)'  
-   elif i == 2 :
-      return '  Cubic (fcc)'  
-   elif i == 3 :
-      return f'  Cubic (bcc): V1 = {a/2}(1, 1, 1), V2 = {a/2}(-1, 1, 1), V3 = {a/2}(-1, -1, 1)'  
-   elif i == -3:
-      return '  Cubic (bcc)'  
-   elif i == 4 :
-      return f'  Hexagonal: V1 = {a}(1, 0, 0), V2 = {a}(-1/2, sqrt(3)/2, 0), V3 = (0, 0, c)'  
-   elif i == 5 :
-      return '  Trigonal'  
-   elif i == -5 :
-      return '  Trigonal'  
-   elif i == 6 :
-      return '  Tetragonal (st)'  
-   elif i == 7 :
-      return '  Tetragonal (bct)'  
-   elif i == 8 :
-      return '  Orthorhombic'  
-   elif i == 9 :
-      return '  Orthorhombic (bco)'  
-   elif i == -9 :
-      return '  Orthorhombic (bco)'  
-   elif i == 91 :
-      return '  Orthorhombic one-face base-centered'  
-   elif i == 10 :
-      return '  Orthorhombic face-centered'  
-   elif i == 11 :
-      return '  Orthorhombic body-centered'   
-   elif i == 12 : 
-      return '  Monoclinic'  
-   elif i == -12 :
-      return '  Monoclinic'  
-   elif i == 13 :
-      return '  Monoclinic (bc)'  
-   elif i == -13 :
-      return '  Monoclinic (bc)'  
-   elif i == 14 :
-      return '  Triclinic'  
-   else :
-      return '  stai sbagliando'  
+    bravais_mapping = {
+        0: '  Free',
+        1: f'  Cubic (sc): V1 = {a}(1, 0, 0), V2 = {a}(0, 1, 0), V3 = {a}(0, 0, 1)',
+        2: '  Cubic (fcc)',
+        3: f'  Cubic (bcc): V1 = {a/2}(1, 1, 1), V2 = {a/2}(-1, 1, 1), V3 = {a/2}(-1, -1, 1)',
+        -3: '  Cubic (bcc)',
+        4: f'  Hexagonal: V1 = {a}(1, 0, 0), V2 = {a}(-1/2, sqrt(3)/2, 0), V3 = (0, 0, c)',
+        5: '  Trigonal',
+        -5: '  Trigonal',
+        6: '  Tetragonal (st)',
+        7: '  Tetragonal (bct)',
+        8: '  Orthorhombic',
+        9: '  Orthorhombic (bco)',
+        -9: '  Orthorhombic (bco)',
+        91: '  Orthorhombic one-face base-centered',
+        10: '  Orthorhombic face-centered',
+        11: '  Orthorhombic body-centered',
+        12: '  Monoclinic',
+        -12: '  Monoclinic',
+        13: '  Monoclinic (bc)',
+        -13: '  Monoclinic (bc)',
+        14: '  Triclinic',
+        -14: '  Triclinic',
+    }
+    return bravais_mapping.get(i, '  stai sbagliando') 
    
 def read_info(fin):
-   for i in fin:
-      if 'number of atoms/cell' in i:
-         match_n_at = re.search(r'-?\d+(\.\d+)?', i)
-         n_at = int(match_n_at.group())
-      if 'bravais-lattice index' in i:
-         match_bli = re.search(r'-?\d+(\.\d+)?', i)
-         bravais = int(match_bli.group())
-      if 'lattice parameter' in i:
-         match_lp = re.search(r'-?\d+(\.\d+)?', i)
-         lattice_param = float(match_lp.group())
-   return [n_at, bravais, lattice_param]
+    n_at, bravais, lattice_param = 0, 0, 0.0
+    for line in fin:
+        if 'number of atoms/cell' in line:
+            n_at = int(re.search(r'-?\d+(\.\d+)?', line).group())
+        elif 'bravais-lattice index' in line:
+            bravais = int(re.search(r'-?\d+(\.\d+)?', line).group())
+        elif 'lattice parameter' in line:
+            lattice_param = float(re.search(r'-?\d+(\.\d+)?', line).group())
+    return n_at, bravais, lattice_param
 
 def first_two_lines(fin):
-   n_at, brav, latt = read_info(fin)
-   return [n_at, ibrav(brav, latt)]
+    n_at, brav, lattice_param = read_info(fin)
+    return [n_at, ibrav(brav, lattice_param)]
 
 def gen_list_for_xyz(filein):
-   fin = open(filein, 'r')
-   store = []
-   for line in fin.read().split('\n'):
-      store.append(line)
-   fin.close()
+    with open(filein, 'r') as fin:
+        store = fin.read().split('\n')
 
-   fstored = first_two_lines(store)
-   t = 0
-   n_at = fstored[0]
-   ctrl = 0
-   counter = 0
-   for i in store:
-      if 'time      =   ' in i:
-         match_t = re.search(r'-?\d+(\.\d+)?', i)
-         t = float(match_t.group())
-      if 'Entering Dynamics:    iteration =' in i:
-         match_N = re.search(r'-?\d+(\.\d+)?', i)
-         N = int(match_N.group())
-      if ctrl > n_at:
-         ctrl = 0
-      if ctrl > 0:
-         fstored.append(i)
-         ctrl += 1
-      if 'ATOMIC_POSITIONS' in i:
-         if N == 1:
-            fstored[1] = f'- t = {t} ps (iter. {N}) -' + fstored[1]
-         else:
-            fstored.append(n_at)
-            fstored.append(f'- t = {t} ps (iter. {N}) -')
-         ctrl = 1
-         counter += 1
-   return(fstored)
+    fstored = first_two_lines(store)
+    t, n_at, ctrl, counter = 0, fstored[0], 0, 0
 
+    for line in store:
+        if 'time      =   ' in line:
+            t = float(re.search(r'-?\d+(\.\d+)?', line).group())
+        elif 'Entering Dynamics:    iteration =' in line:
+            N = int(re.search(r'-?\d+(\.\d+)?', line).group())
+        elif ctrl > n_at:
+            ctrl = 0
+        elif ctrl > 0:
+            fstored.append(line)
+            ctrl += 1
+        elif 'ATOMIC_POSITIONS' in line:
+            if N == 1:
+                fstored[1] = f'- t = {t} ps (iter. {N}) - {fstored[1]}'
+            else:
+                fstored.append(str(n_at))
+                fstored.append(f'- t = {t} ps (iter. {N}) -')
+            ctrl = 1
+            counter += 1
 
-file_to_open=str(input('Insert filename: ____.pwo \n'))
-#file_to_open = 'test'
+    return fstored
 
-fout=open(file_to_open + '.xyz', "w+")
-
-fout.writelines(["%s\n" % i  for i in gen_list_for_xyz(file_to_open + '.pwo')])
-fout.close()
+if __name__ == "__main__":
+    file_to_open = str(input('Insert filename: ____.pwo \n'))
+    with open(file_to_open + '.xyz', "w+") as fout:
+        fout.writelines(["%s\n" % i for i in gen_list_for_xyz(file_to_open + '.pwo')])
