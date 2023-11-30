45ab958686787c1e614267c7e0588e95498f5d4f
Lorenzo Razzolini <lorenzo.razzolini@studio.unibo.it>
2023-11-30 14:07:40 +0100

    generating the new main file pwo_into_xyz.py and build the main structure

--- pwo_into_xyz.py
+++ pwo_into_xyz.py
@@ -1,3 +1,6 @@
+# main
+
+from class_iteration import iteration
 
 def setup():
     '''
@@ -17,18 +20,47 @@ def setup():
 
     return filename
 
-def xyz_lines(fin):
+def xyz_gen(fout, fin):
+    iter = iteration()
+    e_storage = False
+    e_gen = False
+    
     for idx,line in enumerate(fin):
-        fout.writelines()
+        
+        if 'number of atoms/cell' in line:
+            iter.n_at = int(line.split()[4])
+            print(iter.n_at)
+        elif 'a(1)' in line:
+            x, y, z = map(float, line.split()[3:6])
+            iter.ax = [x, y, z]
+            #print(iter.ax)
+        elif 'a(2)' in line:
+            x, y, z = map(float, line.split()[3:6])
+            iter.ay = [x, y, z]
+            #print(iter.ay)
+        elif 'a(3)' in line:
+            x, y, z = map(float, line.split()[3:6])
+            iter.az = [x, y, z]
+            #print(iter.az)
+
+        if 'Self-consistent Calculation' in line:
+            fout.writelines(iter.first_two_lines())
+            if e_gen == True:
+                #calculation
+            e_gen = True
+            e_storage = True
+        
+        if e_storage == True:
+            iter.store(line)
+
+        #Self constant calculation
+
+        #print(n_at, bravais, lattice_param, V)
+        #fout.writelines()
     return
 
 if __name__ == "__main__":
     filename = setup()
     with open(filename + '.xyz', "w+") as fout:
         with open(filename + '.pwo', 'r') as fin:
-            xyz_lines(fin)
-        
-        #usa le variabili ad errore, magari con un dizionario, a cui soino collegate
-        #fout.writelines(["%s\n" % i for i in gen_list_for_xyz(file_to_open + '.pwo')])
-
- #   '''file_to_open = str(input('Insert filename: ____.pwo \n'))'''
+            xyz_gen(fout, fin)
