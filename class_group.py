cfb157bebcd041c84519c541a5e759dcff672b2a
Lorenzo Razzolini <lorenzo.razzolini@studio.unibo.it>
2023-12-03 15:51:48 +0100

    rimnozione di alcuni commenti

--- class_group.py
+++ class_group.py
@@ -4,84 +4,52 @@ import numpy as np
 
 class group:
 
-    id_group = 0
-    id = []
-    N = 0
-    force =  np.array([]).reshape(0, 3)
-    position_past = np.array([]).reshape(0, 3)
-    position = np.array([]).reshape(0, 3)
-    velocity = np.array([]).reshape(0, 3)
-
-    Ek = 0
-    Ftot = [0, 0, 0]
-    T = 0
-    DOF = 0
-
-    Ry_to_eV = 13.60570398  # 1 Ry = 13.60570398 eV
-    eV_j = 1.60e-19         # 1 eV = 1.60e-19 j
-
     def __init__(self, type, mass):
-        #estrai il numero dalla stringa 
         self.type = type
         self.mass = mass
+        self.id_group = 0
         for i in self.type:
             if i.isdigit():
                 self.id_group = int(i)
-
+        self.id = np.array([], dtype=int)
+        self.N = 0
+        self.force = np.array([], dtype=float).reshape(0, 3)
+        self.position_past = np.array([], dtype=float).reshape(0, 3)
+        self.position = np.array([], dtype=float).reshape(0, 3)
+        self.velocity = np.array([], dtype=float).reshape(0, 3)
+        self.Ek = 0
+        self.Ftot = np.array([0, 0, 0], dtype=float)
+        self.T = 0
+        self.DOF = 0
+        
     def add_id(self, i):
         self.id = np.append(self.id, i)
-        return
     
-    def check_id(self, id):
-        return self.id == id
+    '''def check_id(self, id):
+        return self.id == id'''
     
     def add_position_past(self, x, y, z):
         self.position_past = np.vstack([self.position_past, np.array([x, y, z], dtype=float)])
-        return
     
     def add_position(self, x, y, z):
         self.position = np.vstack([self.position, np.array([x, y, z], dtype=float)])
-        return
     
     def add_force(self, x, y, z):
-        #print(self.force)
         self.force = np.vstack([self.force, np.array([x, y, z], dtype=float)])
-        return
-    
-    def Velocity_(self, Dt):
-        #print(np.array(self.position, dtype=float) - np.array(self.position_past, dtype=float))
-        #print(self.position_past, '\n', Dt)
+
+    def Velocity(self, Dt):
         self.velocity = (np.array(self.position, dtype=float) - np.array(self.position_past, dtype=float)) / Dt
 
-        return
-    
     def generate(self, Dt):
-        self.Velocity_(Dt)
-        #print(self.velocity)
-        #print(np.linalg.norm(self.velocity, axis=1), '\n')
-        #print(np.linalg.norm(self.velocity, axis=1)**2, '\n')
+        self.Velocity(Dt)
         self.Ek = 0.5 * float(self.mass) * np.sum(np.linalg.norm(self.velocity, axis=1)**2) * 0.0001036426948415943
-        # (A/ps -> m/s)^2 = 1e4
-        # g/mol -> Kg/mol = 1e-3
-        # eV -> j = 1.602176634e-19
-        # Na = 6.02214086e23
-        # Kb = 1.3806503e-23 jK-1 = 8.617333262145e-5 eVK-1
-        #print(10 /(1.602176634e-19 * 6.02214086e23))
         self.DOF = self.N
-        #print(self.DOF)
         self.T = (2 * self.Ek) / (self.DOF * 8.617333262145e-5)
-        #print('bef')
-        #print(self.velocity)
-        self.Force_tot = np.sum(self.force, axis=0)
-        #print('aft')
-        body = []
-        #print(self.force)
+        self.Ftot = np.sum(self.force, axis=0)
+        body = np.array([], dtype=str)
         for i in range(self.N):
-            #print(f'{self.type}    {self.position[i][0]}   {self.position[i][1]}   {self.position[i][2]}   {self.velocity[i][0]}   {self.velocity[i][1]}   {self.velocity[i][2]}  {self.force[i][0]}   {self.force[i][1]}   {self.force[i][2]}')
             body = np.append(body, f'{self.type}\t  {self.position[i][0]}\t  {self.position[i][1]}\t  {self.position[i][2]}\t  {self.velocity[i][0]}\t  {self.velocity[i][1]}\t  {self.velocity[i][2]}\t  {self.force[i][0]}\t  {self.force[i][1]}\t  {self.force[i][2]}\t  {self.id_group}')
-
         self.position_past = self.position
-        self.force =  np.array([]).reshape(0, 3)
-        self.position = np.array([]).reshape(0, 3)
-
+        self.force = np.array([], dtype=float).reshape(0, 3)
+        self.position = np.array([], dtype=float).reshape(0, 3)
         return body
--- class_iteration.py
+++ class_iteration.py
@@ -4,31 +4,26 @@ import numpy as np
 from class_group import group
 
 class iteration:
-    n_at = 0
-    celldim = 0
-    ax = [0, 0, 0] #A
-    ay = [0, 0, 0] #A
-    az = [0, 0, 0] #A
-    t_past = 0 #ps
-    t = 0 #ps
-    U_pot = 0
-    input = ''
-    block = []
-    groups = []
-    switch = False
 
-    # Conversion
-    Ry_to_eV = 13.60570398  # 1 Ry = 13.60570398 eV
-    #eV_j = 1.60e-19         # 1 eV = 1.60e-19 j
-    bohr_angstrom =	0.529177249 # 1 A = 0.529177249 Bohr
-    alat_to_angstrom = 0
-
-
-    pass
+    def __init__(self):
+        self.n_at = 0
+        self.celldim = 0
+        self.ax = [0, 0, 0]
+        self.ay = [0, 0, 0]
+        self.az = [0, 0, 0]
+        self.t_past = 0
+        self.t = 0
+        self.U_pot = 0
+        self.input = ''
+        self.block = np.array([], dtype=str)
+        self.groups = np.array([], dtype=group)
+        self.switch = False
+        self.Ry_to_eV = 13.60570398
+        self.bohr_angstrom = 0.529177249
+        self.alat_to_angstrom = 0
 
     def Alat_to_Angstrom(self):
         self.alat_to_angstrom = self.celldim * self.bohr_angstrom
-        return 
 
     '''def first_two_lines(self):
         
@@ -56,7 +51,6 @@ class iteration:
         '''
         if line != '':
             self.block = np.append(self.block, line)
-        return
     
     def add_group(self, _type, _mass):#, idx):
         '''
@@ -70,9 +64,7 @@ class iteration:
             None
         '''
         
-        self.groups = np.append(self.groups, group(_type, _mass))#, idx))
-        #print(_mass, _type)#, idx)
-        return
+        self.groups = np.append(self.groups, group(_type, _mass))
     
     def count_group(self, line):
         '''
@@ -87,12 +79,9 @@ class iteration:
         for elm in self.groups:
             if elm.type == line[1]:
                 elm.N += 1
-                #print(line[0])
                 elm.add_id(int(line[0]))
-                #elm.add_position_past(line[6] * self.alat_to_angstrom, line[7] * self.alat_to_angstrom, line[8] * self.alat_to_angstrom)
                 elm.add_position_past(float(line[6]) * self.alat_to_angstrom, float(line[7]) * self.alat_to_angstrom, float(line[8]) * self.alat_to_angstrom)
                 break
-        return
     
     def forces(self, line):
         '''
@@ -106,14 +95,10 @@ class iteration:
         
         '''
         _line = line.split()
-        #print(_line)
         for elm in self.groups:
-            #print(_line[2])
             if int(_line[1]) in elm.id:
-                #print(_line[6], _line[7], _line[8])
                 elm.add_force(_line[6], _line[7], _line[8])
                 break
-        return
     
     def position(self, line):
         '''
@@ -127,41 +112,32 @@ class iteration:
         
         '''
         _line = line.split()
-        #print(_line)
         for elm in self.groups:
             if _line[0] == elm.type:
-                elm.add_position( _line[1], _line[2], _line[3])
+                elm.add_position(_line[1], _line[2], _line[3])
                 break
-        return
 
     def text(self):
-        text = [f'{self.n_at}', 
-                f"Lattice(Ang)=\"{self.ax[0]}, {self.ax[1]}, {self.ax[2]}, {self.ay[0]}, {self.ay[1]}, {self.ay[2]}, {self.az[0]}, {self.az[1]}, {self.az[2]}\" t(ps)={self.t} Epot(eV)={self.U_pot / self.Ry_to_eV }"]
-        body = []
+        text = np.array([f'{self.n_at}', f"Lattice(Ang)=\"{self.ax[0]}, {self.ax[1]}, {self.ax[2]}, {self.ay[0]}, {self.ay[1]}, {self.ay[2]}, {self.az[0]}, {self.az[1]}, {self.az[2]}\" t(ps)={self.t} Epot(eV)={self.U_pot / self.Ry_to_eV }"])
+        body = np.array([], dtype=str)
         for elm in self.groups:
             _body = elm.generate(self.t - self.t_past)
             elm.DOF = elm.N
             text[1] += f" Ek{elm.id_group}(ev)={elm.Ek} DOF{elm.id_group}={elm.DOF} T{elm.id_group}(K)={elm.T} Ftot{elm.id_group}(pN)=\"{elm.Ftot[0]}, {elm.Ftot[1]}, {elm.Ftot[2]}\""
-
             body = np.append(body, _body)
-        
-        text = np.append(text, body)
 
+        text = np.append(text, body)
         return text
 
     def single_frame(self):
         for line in self.block:
-
             if 'time      =' in line:
                 self.t_past = self.t
-                self.t = float(line.split()[2]) #ps 
-
+                self.t = float(line.split()[2])
             if '!    total energy' in line:
-                self.U_pot = float(line.split()[4]) #Ry
-            
+                self.U_pot = float(line.split()[4])
             if '   force =    ' in line:
                 self.forces(line)
-
             if 'ATOMIC_POSITIONS' in line:
                 self.switch = True
             elif self.switch == True:
@@ -170,9 +146,7 @@ class iteration:
                     self.switch = False
                 else:
                     self.position(line)
-        
-        text =self.text() 
 
+        text = self.text()
         self.block = []
-
         return text
